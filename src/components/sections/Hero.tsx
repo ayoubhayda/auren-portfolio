f370dc063f2a1378722fece41ead0bdfa163ad02
Ayoub Hayda <ayoubhayda01@gmail.com>
2025-08-01 12:32:30 +0100

    upload resume url

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -153,7 +153,7 @@ const Hero = () => {
         >
           <motion.div variants={buttonVariants}>
             <Button size="lg" className=" text-white px-6 shadow-none cursor-pointer" asChild>
-              <a href="https://u32tyflba0.ufs.sh/f/kc5DWd3AVQkLcsKZ7YkM1TDQt4evgSYxW7lfu32BjAdPULyO" rel="noreferrer" target={"_blank"} download>
+              <a href="https://u32tyflba0.ufs.sh/f/kc5DWd3AVQkLba2eufGMutRgEr6TP3KoQOCfd2ZlB79WGiwq" rel="noreferrer" target={"_blank"} download>
                 <Download className="mr-1 h-4 w-4" />
                 Download Resume
               </a>
